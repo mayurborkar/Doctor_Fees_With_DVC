304c0007dc65b1e2f21b1b802e2cff360f080735
Mayur Borkar <borkarm112@gmail.com>
2022-03-03 15:50:28 +0530

    requirements add

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
+REPO_NAME = "Doctor_Fees_With_DVC"
 AUTHOR_USER_NAME = "mayurborkar"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
@@ -14,12 +14,12 @@ setup(
     name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="A small package for DVC",
+    description="A small package for Doctor Fees prediction With DVC",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     author_email="borkarm112@gmail.com",
-    packages=[SRC_REPO],
+    packages=find_packages(),
     license="MIT",
     python_requires=">=3.6",
     install_requires=LIST_OF_REQUIREMENTS
