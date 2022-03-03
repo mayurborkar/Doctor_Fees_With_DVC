4efda6ac8e23d3f7a0d9e4b0b63b34fc7008459b
Mayur Borkar <borkarm112@gmail.com>
2022-03-03 16:50:28 +0530

    Stage 1 Get data

---
+++ src/stage_01_get_data.py
@@ -0,0 +1,37 @@
+from src.utils.common import read_yaml, load_model
+import pandas as pd
+import argparse
+import logging
+import yaml
+import os
+
+STAGE = "Get Data" ## <<< change stage name 
+
+logging.basicConfig(
+                    filename=os.path.join("logs", 'running_logs.log'), 
+                    level=logging.INFO, 
+                    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+                    filemode="a"
+                    )
+
+
+def get_data(config_path):
+    config = read_yaml(config_path)
+    data = config['data_source']['source']
+    data = pd.read_excel(data)
+    return data
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        get_data(config_path=parsed_args.config)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
---
+++ src/utils/common.py
@@ -0,0 +1,31 @@
+import logging
+import json
+import yaml
+import os
+import io
+def read_yaml(path_to_yaml: str) -> dict:
+    with open(path_to_yaml) as yaml_file:
+        content = yaml.safe_load(yaml_file)
+    logging.info(f"yaml file: {path_to_yaml} loaded successfully")
+    return content
+
+def create_directories(path_to_directories: list) -> None:
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        logging.info(f"created directory at: {path}")
+
+
+def save_json(path: str, data: dict) -> None:
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+
+    logging.info(f"json file saved at: {path}")
+
+
+def load_model(model):
+    with io.StringIO() as stream:
+        model(
+            print_fn = lambda x: stream.write(f'{x}\n')
+        )
+        summary_str = stream.getvalue()
+    return summary_str
